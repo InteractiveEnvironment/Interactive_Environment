5b30966d520e75b90e9429dc5b17e0303c0cf446
Fabian Bach <bachfa45944@ohm-hochschule.de>
2013-05-12 17:34:00 +0200

    Doxygen Doku
    Erste Doku als HTML erstellt

---
+++ Dokumentation/html/search/functions_6c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['loescheblock',['loescheBlock',['../classkern_interface.html#ad5b72cb0f1db0e9788c73f1e6429407f',1,'kernInterface']]],
+  ['loescheverbindung',['loescheVerbindung',['../classkern_interface.html#aad2cbb4b73b98e12a27041718948b611',1,'kernInterface']]]
+];
